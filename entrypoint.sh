7719fd2bc0a172016fc1c083a0fb62b65d01464f
Lam Huynh <thanhlam0290@gmail.com>
2025-11-05 13:41:16 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ service nginx start
 nginx -v
 service nginx status
 rm -f /tmp/.wrapper*
-chown -R fred:fred /conf /data /download /fred
-exec gosu fred /fred/docker-run
+chown -R 1000:1000 /conf /data /download /fred
+exec gosu 1000 /fred/docker-run
